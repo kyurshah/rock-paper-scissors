edee97253709c7abd1a894ba0b8962fd64d33ab3
keyur <13107634+kyurshah@users.noreply.github.com>
2024-11-22 23:06:45 +0530

    Add game logic

--- script.js
+++ script.js
@@ -25,53 +25,53 @@ function getHumanChoice() {
 }
 
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+function playGame() {
 
-    if (humanChoice == "rock" && computerChoice == "paper") {
-        computerScore += 1;
-        return "Computer Wins!";
-    }
-    else if (humanChoice == "rock" && computerChoice == "scissors") {
-        humanScore += 1;
-        return "You Win!";
-    }
-    else if (humanChoice == "rock" && computerChoice == "rock") {
-        computerScore += 1;
-        humanScore += 1;
-        return "TIE!";
-    }
-
-    else if (humanChoice == "paper" && computerChoice == "paper") {
-        computerScore += 1;
-        humanScore += 1;
-        return "TIE";
-    }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
-        computerScore += 1;
-        return "Computer Win!";
-    }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-
-        humanScore += 1;
-        return "You Win!";
-    }
-
-    if (humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore += 1;
-        return "You Wins!";
-    }
-    else if (humanChoice == "scissors" && computerChoice == "scissors") {
-        humanScore += 1;
-        computerScore += 1;
-        return "TIE!";
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        computerScore += 1;
-        return "Computer Wins!";
+    for (let i = 0 ; i < 5 ; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRound(humanChoice, computerChoice)
+        console.log(`You chose ${humanChoice} while Computer chose ${computerChoice}`)
+        console.log(`${result}`)
+        console.log(`Your score is ${humanScore} and Computer score is ${computerScore}`)
     }
 
 
+
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            computerScore += 1;
+            humanScore += 1;
+            return `Its a TIE - ${humanChoice} = ${computerChoice}`;
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            computerScore += 1;
+            return `Computer Wins - ${computerChoice} beats ${humanChoice}`;
+        }
+        else if (humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore += 1;
+            return `You Win - ${humanChoice} beats ${computerChoice}`;
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore += 1;
+            return `Computer Wins - ${computerChoice} beats ${humanChoice}`;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+    
+            humanScore += 1;
+            return `You Win - ${humanChoice} beats ${computerChoice}`;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore += 1;
+            return `You Win - ${humanChoice} beats ${computerChoice}`;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            computerScore += 1;
+            return `Computer Wins - ${computerChoice} beats ${humanChoice}`;
+        }
+    }
 
 }
+
+playGame();
